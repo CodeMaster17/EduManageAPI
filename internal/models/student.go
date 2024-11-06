4d6011ee5242171b4cbb5cc67d893196d596b212
Harshit Yadav <2105891@kiit.ac.in>
2024-11-06 12:07:51 +0530

    fix: id converted to string

--- internal/models/student.go
+++ internal/models/student.go
@@ -1,8 +1,8 @@
 package models
 
 type Student struct {
-    ID    int    `json:"id"`
-    Name  string `json:"name"`
-    Age   int    `json:"age"`
-    Email string `json:"email"`
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
 }
